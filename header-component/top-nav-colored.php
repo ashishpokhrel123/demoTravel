4534d3797c155372209c6bc898a074ac5f90048c
nirajan13 <nirajandahal22@gmail.com>
2019-12-28 22:15:29 +0545

    top nav added

---
+++ header-component/top-nav-colored.php
@@ -0,0 +1,20 @@
+<div class="top-nav py-4">
+	<div class="container-fluid px-5 d-flex">
+		<div class="top-nav-left mr-auto">
+			<span><i class="fa fa-phone mr-2 primary-color" aria-hidden="true"></i> +977 9862155336</span>
+			<span class="ml-4"> <i class="fa fa-envelope mr-2 primary-color" aria-hidden="true"></i> +977 9862155336</span>
+		</div>
+		<div class="top-nav-right d-flex">
+			<div class="social mr-3">
+				<span class="mr-1"><i class="fa fa-facebook mr-1 primary-color" aria-hidden="true"></i></span>
+				<span class="mr-1"><i class="fa fa-twitter mr-1 primary-color" aria-hidden="true"></i></span>
+				<span class="mr-1"><i class="fa fa-instagram mr-1 primary-color" aria-hidden="true"></i></span>
+				<span class="mr-1"><i class="fa fa-pinterest mr-1 primary-color" aria-hidden="true"></i></span>
+			</div>
+			<div class="cred pl-3">
+				<span class="mr-3"><i class="fa fa-lock mr-2 primary-color" aria-hidden="true"></i> Login</span>
+				<span><i class="fa fa-user mr-2 primary-color" aria-hidden="true"></i> Register</span>
+			</div>
+		</div>
+	</div>
+</div>
---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Travel Site</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap-4.min.css">
+	<link rel="stylesheet" type="text/css" href="css/slick-slider.css">
+	<link rel="stylesheet" type="text/css" href="css/animate.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<!-- <link rel="stylesheet" type="text/css" href="css/font-awesome.css"> -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+</head>
+<body>
+	
+	<header>
+		<div>
+			<?php include('header-component/top-nav-colored.php'); ?>
+			  <div class="menu">
+			    <ul class="d-flex flex-column">
+			       <li><a href=""><i class="fa fa-terminal"></i>home</a></li>
+			       <li><a href=""><i class="fa fa-codepen"></i>about</a></li>
+			       <li><a href=""><i class="fa fa-newspaper-o"></i>news</a></li>
+			       <li><a href=""><i class="fa fa-paper-plane-o"></i>contact</a></li>
+			    </ul>
+			  </div>
+			  <div class="content">
+			    <div class="button">
+			      <div class="line first top"></div>
+			      <div class="line second bottom"></div>
+			    </div>
+			  </div>
+		</div>		
+	</header>
+
+	<section>
+		this is the container for travel site
+	</section>
+	<footer>
+		this is the footer section
+	</footer>
+
+	<script src="js/jquery-3.3.1.js"></script>
+	<script src="js/popper.min.js"></script>
+
+	<script src="js/bootstrap-4.min.js"></script>
+
+	<script>
+      	new WOW().init();
+    </script>
+
+    <script>
+    	function expand(){
+		  $(this).toggleClass("on");
+		  $(".menu").toggleClass("active");
+		};
+		$(".button").on('click', expand);
+    </script>
+	
+</body>
+</html>
